feae077d3753ffc2175ffbab25ccfde0d33c6a67
gra4evp <petrgrachev1@gmail.com>
2024-06-11 18:17:06 +0300

    add doc_strings

--- src/image_processing.py
+++ src/image_processing.py
@@ -4,23 +4,64 @@ from logger import logger
 
 
 def scale_frame(frame, scale):
+    """
+    Масштабирует изображение (frame) до указанного коэффициента (scale).
+
+    Parameters:
+    - frame: numpy.ndarray, входное изображение.
+    - scale: float, коэффициент масштабирования.
+
+    Returns:
+    - numpy.ndarray: масштабированное изображение.
+    """
     height, width = frame.shape[:2]
     dim = (int(width * scale), int(height * scale))
     return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
 
 
 def crop_frame(frame, bbox):
+    """
+    Обрезает изображение по заданным координатам (bbox).
+
+    Parameters:
+    - frame: numpy.ndarray, входное изображение.
+    - bbox: tuple[int], координаты обрезки (h1, h2, w1, w2).
+
+    Returns:
+    - numpy.ndarray: обрезанное изображение.
+    """
     h1, h2, w1, w2 = bbox
     return frame[h1:h2, w1:w2]
 
 
 def transform_frame(frame, transforms):
+    """
+    Применяет последовательность преобразований к изображению.
+
+    Parameters:
+    - frame: numpy.ndarray, входное изображение.
+    - transforms: list[tuple[callable, dict]], список преобразований и их аргументов.
+
+    Returns:
+    - numpy.ndarray: преобразованное изображение.
+    """
     for transform, args in transforms:
         frame = transform(frame, **args)
     return frame
 
 
 def save_face_images(imgs, filenames, dirpath):
+    """
+    Сохраняет изображения лиц в указанную директорию.
+
+    Parameters:
+    - imgs: list[numpy.ndarray], список изображений лиц.
+    - filenames: list[str], список имен файлов.
+    - dirpath: str, путь к директории для сохранения изображений.
+
+    Returns:
+    - None
+    """
     for img, filename in zip(imgs, filenames):
         filepath = os.path.join(dirpath, filename)
         cv2.imwrite(filename=filepath, img=img)
@@ -38,8 +79,7 @@ def draw_boxes(frame, boxes):
     Returns:
     - frame_with_boxes: изображение с нарисованными прямоугольниками
     """
-    # Копируем изображение, чтобы сохранить оригинал
-    frame_with_boxes = frame.copy()
+    frame_with_boxes = frame.copy()  # Копируем изображение, чтобы сохранить оригинал
 
     for box in boxes:
         x1, y1, x2, y2 = box
