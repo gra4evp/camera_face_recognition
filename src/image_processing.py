92cec7bcbd3dfffa2665c5165c405706f24fcad8
gra4evp <petrgrachev1@gmail.com>
2024-06-19 18:55:31 +0300

    add transform classes

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,6 +1,114 @@
 import cv2
 import os
+import numpy as np
 from logger import collogger
+
+from typing import Tuple, List, Callable
+
+
+class ScaleFrame:
+    """
+    Класс для масштабирования изображения.
+
+    Parameters:
+    - scale (float): Коэффициент масштабирования.
+    """
+    scale: float
+
+    def __init__(self, scale: float):
+        self.scale = scale
+
+    def __call__(self, frame: np.ndarray) -> np.ndarray:
+        """
+        Масштабирует изображение до указанного коэффициента.
+
+        Parameters:
+        - frame (numpy.ndarray): Входное изображение.
+
+        Returns:
+        - numpy.ndarray: Масштабированное изображение.
+        """
+        height, width = frame.shape[:2]
+        dim = (int(width * self.scale), int(height * self.scale))
+        return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+
+
+class CropFrame:
+    """
+    Класс для обрезки изображения.
+
+    Parameters:
+    - bbox (tuple[int, int, int, int]): Координаты обрезки (h1, h2, w1, w2).
+    """
+    def __init__(self, bbox: Tuple[int, int, int, int]):
+        self.bbox = bbox
+
+    def __call__(self, frame: np.ndarray) -> np.ndarray:
+        """
+        Обрезает изображение по заданным координатам.
+
+        Parameters:
+        - frame (numpy.ndarray): Входное изображение.
+
+        Returns:
+        - numpy.ndarray: Обрезанное изображение.
+        """
+        h1, h2, w1, w2 = self.bbox
+        return frame[h1:h2, w1:w2]
+
+
+class Canny:
+    """
+    Класс для применения Canny edge detection к изображению.
+
+    Parameters:
+    - threshold1 (int): Нижний порог для гистерезиса.
+    - threshold2 (int): Верхний порог для гистерезиса.
+    """
+
+    def __init__(self, threshold1: int = 100, threshold2: int = 200):
+        self.threshold1 = threshold1
+        self.threshold2 = threshold2
+
+    def __call__(self, frame: np.ndarray) -> np.ndarray:
+        """
+        Применяет Canny edge detection к изображению.
+
+        Parameters:
+        - frame (numpy.ndarray): Входное изображение.
+
+        Returns:
+        - numpy.ndarray: Изображение с применением Canny edge detection.
+        """
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray_frame, self.threshold1, self.threshold2)
+        return edges
+
+
+class Compose:
+    """
+    Класс для последовательного применения нескольких трансформаций.
+
+    Parameters:
+    - transforms (list[callable]): Список трансформаций для применения.
+    """
+
+    def __init__(self, transforms: List[Callable]):
+        self.transforms = transforms
+
+    def __call__(self, frame: np.ndarray) -> np.ndarray:
+        """
+        Последовательно применяет трансформации к изображению.
+
+        Parameters:
+        - frame (numpy.ndarray): Входное изображение.
+
+        Returns:
+        - numpy.ndarray: Преобразованное изображение.
+        """
+        for transform in self.transforms:
+            frame = transform(frame)
+        return frame
 
 
 def scale_frame(frame, scale):
@@ -32,6 +140,12 @@ def crop_frame(frame, bbox):
     """
     h1, h2, w1, w2 = bbox
     return frame[h1:h2, w1:w2]
+
+
+def canny(frame, threshold1=100, threshold2=200):
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray_frame, threshold1, threshold2)
+    return edges
 
 
 def transform_frame(frame, transforms):
