453c192e57660bbde77cd263b2d1d4d541374263
gra4evp <petrgrachev1@gmail.com>
2024-06-19 15:07:32 +0300

    refactoring and add class FaceDetection

--- src/face_detection.py
+++ src/face_detection.py
@@ -1,29 +1,45 @@
 import torch
 from facenet_pytorch import MTCNN
+from typing import Tuple, List, Optional, Dict, Any
 
 
-def detect_faces(frame, landmarks=False):
-    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    # frame = Image.fromarray(frame)
+class BaseFaceDetector:
+    device: str | None
+    model: Any
 
-    # Если landmarks=True, то вернется кортеж длиной 3
-    boxes, probs = mtcnn.detect(frame, landmarks=landmarks)
-    return boxes, probs
+    def __init__(self, identify_device=True):
+        self.device = None
+        if identify_device:
+            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        self.model = None
+
+    def detect_faces(self, frame, landmarks=False):
+        raise NotImplementedError("This method should be overridden in subclasses")
+
+    def process_frame(self, frame, frame_count):
+        boxes, probs = self.detect_faces(frame)
+        detected_faces = {'boxes': [], 'images': [], 'filenames': []}
+        if boxes is not None:
+            for person_idx, box in enumerate(boxes):
+                x1, y1, x2, y2 = [int(b) for b in box]
+                face_img = frame[y1:y2, x1:x2]
+
+                detected_faces['boxes'].append((x1, y1, x2, y2))
+                detected_faces['images'].append(face_img)
+                detected_faces['filenames'].append(f"face_frame{frame_count:04}_person{person_idx:02}.jpg")
+        return detected_faces
 
 
-def process_frame(frame, frame_count):
-    boxes, probs = detect_faces(frame)
-    detected_faces = {'boxes': [], 'images': [], 'filenames': []}
-    if boxes is not None:
-        for person_idx, box in enumerate(boxes):
-            x1, y1, x2, y2 = [int(b) for b in box]
-            face_img = frame[y1:y2, x1:x2]
+class MTCNNFaceDetector(BaseFaceDetector):
+    def __init__(self, identify_device=False):
+        super().__init__(identify_device)
+        self.model = MTCNN(keep_all=True, device=self.device)
 
-            detected_faces['boxes'].append((x1, y1, x2, y2))
-            detected_faces['images'].append(face_img)
-            detected_faces['filenames'].append(f"face_frame{frame_count:04}_person{person_idx:02}.jpg")
-    return detected_faces
+    def detect_faces(self, frame, landmarks=False):
+        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # frame = Image.fromarray(frame)
 
-
-DEVICE = device='cuda' if torch.cuda.is_available() else 'cpu'
-mtcnn = MTCNN(keep_all=True, device=DEVICE)
+        # Если landmarks=True, то вернется кортеж длиной 3
+        boxes, probs = self.model.detect(frame, landmarks=landmarks)
+        return boxes, probs
