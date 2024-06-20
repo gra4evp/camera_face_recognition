d86ab866fe4f7b5d5994aa2fd00597222f377d14
gra4evp <petrgrachev1@gmail.com>
2024-06-20 18:58:30 +0300

    add embbedding attr

--- src/db/models.py
+++ src/db/models.py
@@ -12,17 +12,19 @@ class DetectedFace(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     filename = db.Column(db.String, nullable=False)
     image = db.Column(db.LargeBinary, nullable=False)
+    embedding = db.Column(db.LargeBinary, nullable=False)
 
     def __repr__(self):
         return f"<DetectedFace(id={self.id}, filename='{self.filename}')>"
 
 
-def save_faces_to_db(imgs, filenames):
-    for img, filename in zip(imgs, filenames):
+def save_faces_to_db(imgs, filenames, embeddings):
+    for img, filename, embedding in zip(imgs, filenames, embeddings):
         success, face_img_encoded = cv2.imencode('.jpg', img)
         if success:
             face_img_bytes = face_img_encoded.tobytes()
-            db_face = DetectedFace(filename=filename, image=face_img_bytes)
+            embedding_bytes = embedding.tobytes()
+            db_face = DetectedFace(filename=filename, image=face_img_bytes, embedding=embedding_bytes)
             db.session.add(db_face)
             db.session.commit()
             db.session.refresh(db_face)
